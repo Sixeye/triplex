9e47d37932b6e0a49fa70e47e25a48b1099f5476
Sixeye <srinath@srinath.fr>
2019-12-01 18:37:20 +0100

    before functions

--- triplex.cpp
+++ triplex.cpp
@@ -10,11 +10,9 @@ int main()
     std::cout << "Enter the correct code to continue...";
     std::cout << std::endl;
 
-    // Declaring things
-    const int CodeA = 4;
-    const int CodeB = 9;
-    const int CodeC = 7;
-
+    int CodeA = 5;
+    int CodeB = 4;
+    int CodeC = 3;
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
@@ -23,6 +21,28 @@ int main()
     std::cout << "+ There are 3 numbers in the code" << std::endl;
     std::cout << "+ The codes add-up to: "<< CodeSum << std::endl; 
     std::cout << "+ The codes multiply to give: "<< CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+    std::cout << std::endl;
+    std::cout << "Enter A" << std::endl;
+    std::cin >> GuessA; 
+    std::cout << "Enter B" << std::endl;
+    std::cin >> GuessB;
+    std::cout << "Enter C" << std::endl;
+    std::cin >> GuessC;
+    std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "You win!" << std::endl;
+    } else
+    {
+        std::cout << "You loose, try another time !" <<std::endl;
+    }
+    
   
     return 0;
 }
