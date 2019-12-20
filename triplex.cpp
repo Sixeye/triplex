f7444dce56de534e2bdc690aff00c545d661dcf9
Sixeye <srinath@srinath.fr>
2019-12-20 13:28:49 +0100

    End Game

--- triplex.cpp
+++ triplex.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
+#include <ctime>
 
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
     std::cout << "\n ^(°v°)^ \n";
 
     // Amazing beginning sentences
 
-    std::cout << std::endl;
-    std::cout << "You are a secret agent breaking into a secure server room...";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue...\n\n";
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure server room... \nEnter the correct code to continue...\n\n";
 }
 
-void PlayGame()
+bool PlayGame(int Difficulty)
 {
 
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
 
-    int CodeA = 5;
-    int CodeB = 4;
-    int CodeC = 3;
+    int CodeA = rand() % Difficulty + Difficulty;
+    int CodeB = rand() % Difficulty + Difficulty;
+    int CodeC = rand() % Difficulty + Difficulty;
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Showing some things here
     std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code \n";
-    std::cout << "+ The codes add-up to: "<< CodeSum << std::endl; 
-    std::cout << "+ The codes multiply to give: "<< CodeProduct << std::endl;
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: "<< CodeSum; 
+    std::cout << "\n+ The codes multiply to give: "<< CodeProduct;
+     std::cout << std::endl;
 
     // Player's guess
     int GuessA, GuessB, GuessC;
     std::cout << std::endl;
-    std::cout << "Enter A" << std::endl;
-    std::cin >> GuessA; 
-    std::cout << "Enter B" << std::endl;
-    std::cin >> GuessB;
-    std::cout << "Enter C" << std::endl;
-    std::cin >> GuessC;
+    std::cout << "Enter A B and C" << std::endl;
+    std::cin >> GuessA >> GuessB >> GuessC;
     std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << "\n";
 
     int GuessSum = GuessA + GuessB + GuessC;
@@ -47,16 +43,34 @@ void PlayGame()
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
         std::cout << "You win! Little \"Rookie\" you !\n";
+        return true;
+
     } else
     {
-        std::cout << "You loose, try another time !\n";
+        std::cout << "You loose, try another time 007 !\n";
+        return false;
     }
 
 }
 
 int main()
 {
+    srand(time(NULL));
+    int LevelDifficulty = 1;
+    int const MaxDifficulty = 5;
 
-    PlayGame();
+    while (LevelDifficulty <= MaxDifficulty) // Loop game untill all levels completed
+    {
+        bool blevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (blevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+        
+    }
+    std::cout << "\n Great Scott ! You did it John Snow! \n Welcome onboard !\n";
     return 0;
 }
