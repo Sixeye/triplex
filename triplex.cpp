aee1649ce9e16d17aa5ebeade5ebeeb9eee95791
Sixeye <srinath@srinath.fr>
2019-12-20 07:32:37 +0100

    Triple XXX adeventure

--- triplex.cpp
+++ triplex.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
+    std::cout << "\n ^(°v°)^ \n";
+
     // Amazing beginning sentences
 
     std::cout << std::endl;
     std::cout << "You are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
-    std::cout << "Enter the correct code to continue...";
-    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue...\n\n";
+}
+
+void PlayGame()
+{
+
+    PrintIntroduction();
 
     int CodeA = 5;
     int CodeB = 4;
@@ -18,10 +25,11 @@ int main()
 
     // Showing some things here
     std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ There are 3 numbers in the code \n";
     std::cout << "+ The codes add-up to: "<< CodeSum << std::endl; 
     std::cout << "+ The codes multiply to give: "<< CodeProduct << std::endl;
 
+    // Player's guess
     int GuessA, GuessB, GuessC;
     std::cout << std::endl;
     std::cout << "Enter A" << std::endl;
@@ -30,19 +38,25 @@ int main()
     std::cin >> GuessB;
     std::cout << "Enter C" << std::endl;
     std::cin >> GuessC;
-    std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
+    std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << "\n";
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    //check if the player's guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "You win!" << std::endl;
+        std::cout << "You win! Little \"Rookie\" you !\n";
     } else
     {
-        std::cout << "You loose, try another time !" <<std::endl;
+        std::cout << "You loose, try another time !\n";
     }
-    
-  
+
+}
+
+int main()
+{
+
+    PlayGame();
     return 0;
 }
